c4d148e0f0f559611783f3ad0ff54f71f18f5327
Vlad100001 <132137476+Vlad100001@users.noreply.github.com>
2023-05-27 02:18:07 +0300

    Add files via upload

---
+++ ex6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+if ((a % 2) == 0)
+    Console.WriteLine("четное ");
+else 
+    Console.WriteLine("нечетное");
+
