dc0dcab158dc8e9609c0899a436f7a602d140e0e
Vlad100001 <132137476+Vlad100001@users.noreply.github.com>
2023-05-27 02:13:26 +0300

    Add files via upload

---
+++ ex2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите 1 число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2 число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a>b)
+    Console.WriteLine($"max, {a} ");
+else
+    Console.WriteLine($"max, {b} ");
