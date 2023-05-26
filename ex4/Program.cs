0fda9ece668bfe65ca24c8a1b75321d415b2ef06
Vlad100001 <132137476+Vlad100001@users.noreply.github.com>
2023-05-27 02:17:17 +0300

    Add files via upload

---
+++ ex4/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите 1 число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 2 число: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите 3 число: ");
+int c = int.Parse(Console.ReadLine()!);
+if (a>b && a>c)
+    Console.WriteLine($"max= {a} ");
+else if (b>c)
+    Console.WriteLine($"max = {b} ");
+    else  
+    Console.WriteLine($"max = {c} ");
