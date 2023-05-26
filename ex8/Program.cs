7d345609cc9662f525818922797fe08235d157e1
Vlad100001 <132137476+Vlad100001@users.noreply.github.com>
2023-05-27 02:18:38 +0300

    Add files via upload

---
+++ ex8/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a>1)
+    {
+    for (int i = 2; i <= a;  i = i+2)
+    Console.Write($"{i} ");}  
+else
+    Console.WriteLine("ошибка");
